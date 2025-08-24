9486611776302ee3706afae9eecc86d5bc54f499
Wahab Muhammad <wahabmuhammad95@gmail.com>
2025-08-24 22:01:17 +0700

    update useful link dan fasilitas layanan

--- resources/views/public/footer.blade.php
+++ resources/views/public/footer.blade.php
@@ -27,30 +27,32 @@
                 <div class="col-lg-2 col-md-6 footer-links">
                     <h4>Useful Links</h4>
                     <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Beranda</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Rumah Sakit Kami</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Center Of Excellence</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Cari Dokter</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Jadwal Dokter</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Karir</a></li>
                     </ul>
                 </div>
 
                 <div class="col-lg-3 col-md-6 footer-links">
-                    <h4>Our Services</h4>
+                    <h4>Fasilitas dan Layanan</h4>
                     <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Diagnostic Centre</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Intensive Care</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Rawat Inap</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Farmasi</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Emergency</a></li>
                     </ul>
                 </div>
 
                 <div class="col-lg-4 col-md-6 footer-newsletter">
-                    <h4>Our Newsletter</h4>
-                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                    <h4>Dapatkan Kabar Terbaru dari RS Sarkies 'Aisyiyah Kudus</h4>
+                    <p>Daftarkan e-mail Anda untuk berlangganan newsletter dan mendapatkan informasi terbaru dari RS Sarkies 'Aisyiyah Kudus</p>
                     <form action="" method="post">
-                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                        <input type="email" name="email" placeholder="Daftarkan e-mail Anda">
+                        <input type="submit" value="Subscribe">
                     </form>
 
                 </div>
