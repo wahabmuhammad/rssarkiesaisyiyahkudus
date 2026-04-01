894f80741c058f30d396867a9b8cb7f584962017
Wahab <wahabmuhammad95@gmail.com>
2026-04-01 09:48:03 +0700

    update minor

--- resources/views/invitations/invite.blade.php
+++ resources/views/invitations/invite.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Undangan Silaturahim Syawalan — Holding RS Aisyiyah Group Kudus {{ $data['nama'] }}</title>
+    <title>Undangan Silaturahim Syawalan — Holding RS Aisyiyah Group Kudus ({{ $data['nama'] }}) </title>
     <link
         href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400&family=Cinzel:wght@400;600;700&family=Nunito:wght@300;400;600;700&display=swap"
         rel="stylesheet">
